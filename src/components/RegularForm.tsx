e814bf2ae3b0e7de84d9b0d3b654392329c0f09f
Smuel18 <shh92533@gmail.com>
2023-09-28 14:07:02 +0300

    חג שמח

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -1,63 +1,132 @@
-import  { useState, ChangeEvent, FormEvent } from 'react';
-
-interface FormData {
-  username: string;
-  email: string;
-  password:string;
-}
+// import { useState, ChangeEvent, FormEvent } from "react";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 function RegularForm() {
-  const [formData, setFormData] = useState<FormData>({
-    username: '',
-    email: '',
-    password: '',
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      phone: "",
+    },
   });
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert(JSON.stringify(formData));
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-        <h1>Change Me To React Hook Form</h1>
+    <form
+      onSubmit={handleSubmit((data) => {
+        console.log(data), alert(JSON.stringify(data));
+      })}
+    >
+      <h1>Change Me To React Hook Form</h1>
       <div>
         <input
           type="text"
           id="username"
-          name="username"
-          placeholder='Enter UserName'
-          value={formData.username}
-          onChange={handleChange}
+          {...register("username", {
+            required: "שדה זה הוא שדה חובה",
+            minLength: {
+              value: 2,
+              message: "הזן מינימום 2 תווים",
+            },
+          })}
+          placeholder="Enter UserName"
         />
+        {/* <ErrorMessage
+          name="email"
+          errors={errors}
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => (
+              <p key={type}>{message}</p>
+            ))
+          }
+        /> */}
+        {errors.username && <span>{errors.username.message}</span>}
       </div>
+
       <div>
         <input
           type="text"
           id="email"
-          name="email"
-          placeholder='Enter Email'
-          value={formData.email}
-          onChange={handleChange}
+          {...register("email", {
+            required: "שדה זה הוא שדה חובה",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "אנא הזן כתובת אימייל חוקית",
+            },
+          })}
+          placeholder="Enter Email"
         />
+        {/* <ErrorMessage
+          name="email"
+          errors={errors}
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => (
+              <p key={type}>{message}</p>
+            ))
+          }
+        /> */}
+        {errors.email && <span>{errors.email.message}</span>}
       </div>
       <div>
         <input
           type="text"
           id="password"
-          name="password"
-          placeholder='Enter Password'
-          value={formData.password}
-          onChange={handleChange}
+          {...register("password", {
+            required: "שדה זה הוא שדה חובה",
+
+            pattern: {
+              value:
+                /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z0-9]).{8,20}$/,
+              message: "הסיסמה לא תקינה",
+            },
+          })}
+          placeholder="Enter Password"
         />
+        {/* <ErrorMessage
+          name="password"
+          errors={errors}
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => (
+              <p key={type}>{message}</p>
+            ))
+          }
+        /> */}
+
+        {errors.password && <span>{errors.password.message}</span>}
+      </div>
+      <div>
+        <input
+          type="text"
+          id="phone"
+          {...register("phone", {
+            required: "שדה זה הוא שדה חובה",
+            pattern: {
+              value: /^\d{10}$/, // תבנית למספר טלפון באורך של 10 ספרות
+              message: "אנא הזן מספר טלפון חוקי (10 ספרות)",
+            },
+          })}
+          placeholder="Enter a phone number"
+        />
+        {/* <ErrorMessage
+          name="phone"
+          errors={errors}
+          render={({ messages }) =>
+            messages &&
+            Object.entries(messages).map(([type, message]) => (
+              <p key={type}>{message}</p>
+            ))
+          }
+        /> */}
+
+        {errors.phone && <span>{errors.phone.message}</span>}
       </div>
       <button type="submit">Submit</button>
     </form>
